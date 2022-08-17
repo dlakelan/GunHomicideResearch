b1b47434690f0df15e05a029e952f9a9b9c0596b
Daniel Lakeland <dlakelan@street-artists.org>
2022-08-17 13:01:18 -0700

    fix pums download

--- getpums.jl
+++ getpums.jl
@@ -2,16 +2,32 @@
 
 using Downloads
 
+function geturlh(yr)
+    if yr < 2007
+        return "https://www2.census.gov/programs-surveys/acs/data/pums/$i/csv_hus.zip"
+    else
+        return "https://www2.census.gov/programs-surveys/acs/data/pums/$i/1-year/csv_hus.zip"
+    end
+end
+function geturlp(yr)
+    if yr < 2007
+        return "https://www2.census.gov/programs-surveys/acs/data/pums/$i/csv_pus.zip"
+    else
+        return "https://www2.census.gov/programs-surveys/acs/data/pums/$i/1-year/csv_pus.zip"
+    end
+end
+
+
 let f = ""
     for i in 2000:2020
         mkpath("data/pums/$i"; mode = 0o750)
         f = "data/pums/$i/csv_hus.zip"
         if !isfile(f)
-            Downloads.download("https://www2.census.gov/programs-surveys/acs/data/pums/$i/csv_hus.zip",f)
+            Downloads.download(geturlh(i),f)
         end
         f = "data/pums/$i/csv_pus.zip"
         if !isfile(f)
-            Downloads.download("https://www2.census.gov/programs-surveys/acs/data/pums/$i/csv_pus.zip",f)
+            Downloads.download(geturlp(i),f)
         end
     end
 
