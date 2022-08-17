17de4a108145ba715a30b225e4669456932631b4
Daniel Lakeland <dlakelan@street-artists.org>
2022-08-16 17:51:29 -0700

    fix issues in getpums

--- getpums.jl
+++ getpums.jl
@@ -3,15 +3,16 @@
 using Downloads
 
 let f = ""
+    for i in 2000:2020
+        mkpath("data/pums/$i"; mode = 0o750)
+        f = "data/pums/$i/csv_hus.zip"
+        if !isfile(f)
+            Downloads.download("https://www2.census.gov/programs-surveys/acs/data/pums/$i/csv_hus.zip",f)
+        end
+        f = "data/pums/$i/csv_pus.zip"
+        if !isfile(f)
+            Downloads.download("https://www2.census.gov/programs-surveys/acs/data/pums/$i/csv_pus.zip",f)
+        end
+    end
 
-for i in 2000:2020
-    f = "data/pums/$i/csv_hus.zip"
-    if !isfile(f)
-        download("https://www2.census.gov/programs-surveys/acs/data/pums/$i/csv_hus.zip",f)
-    end
-    f = "data/pums/$i/csv_pus.zip"
-    if !isfile(f)
-        download("https://www2.census.gov/programs-surveys/acs/data/pums/$i/csv_pus.zip",f)
-    end
 end
-
